3b95a50e6252f4a3aeb3a885d9085db3ac30f0fb
zhenghaohui <zhenghaohui.cn@gmail.com>
2025-05-11 15:32:09 +0800

    Add tracing and logging utilities for code execution
    Introduce a `Painter` class and related utilities to trace variable changes and log function calls, enhancing debugging capabilities. Include a `CMakeLists.txt` and test cases in `main_test.cpp` to demonstrate functionality.

---
+++ main_test.cpp
@@ -0,0 +1,40 @@
+
+#include "tyan.h" /* <cxx_source> */
+  #include "tyan.h" /* <include> */
+  #include <cassert> /* <include> */
+  #include <chrono> /* <include> */
+  #include <iostream> /* <include> */
+  #include <map> /* <include> */
+  #include <sstream> /* <include> */
+  #include <string> /* <include> */
+  #include <utility> /* <include> */
+  int add(int a, int b){ /* <function> */
+      /* params: a, b */
+      tyan::Painter& tyan_painter = tyan::Painter::get();
+      tyan::PainterDomainGuard tyan_domain_guard_1(&tyan_painter);
+      TyanCatch(a);
+      TyanCatch(b);
+      LogLine("int add(int a, int b){");
+      return a + b; /* <single-sentence> */
+  }
+int fibonacci(int n){ /* <function> */
+      /* params: n */
+      tyan::Painter& tyan_painter = tyan::Painter::get();
+      tyan::PainterDomainGuard tyan_domain_guard_2(&tyan_painter);
+      TyanCatch(n);
+      LogLine("int fibonacci(int n){");
+      if (n <= 1){ /* <if> */
+          return n; /* <single-sentence> */
+      }
+      return fibonacci(n - 1) + fibonacci(n - 2); /* <single-sentence> */
+  }
+int main(){ /* <function> */
+      /* params:  */
+      tyan::Painter& tyan_painter = tyan::Painter::get();
+      tyan::PainterDomainGuard tyan_domain_guard_3(&tyan_painter);
+      LogLine("int main(){");
+      assert(add(10, 20) == 30); /* <assert> */
+      int result = fibonacci(5); /* <var_set> */
+      std::cout << "The 10th Fibonacci number is: " << result << std::endl; /* <single-sentence> */
+      return 0; /* <single-sentence> */
+  }
---
+++ tyan.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <stdint-gcc.h>
+
+namespace tyan {
+    inline std::string to_string(const int num) {
+        return std::to_string(num);
+    }
+
+    class Painter {
+        uint32_t depth_ = 0;
+    public:
+        std::map<std::string, std::string> var_map;
+
+        void push() {
+            depth_ += 1;
+        }
+
+        void pop() {
+            depth_ -= 1;
+        }
+
+
+        static Painter &get() {
+            thread_local Painter painter;
+            return painter;
+        }
+
+        template<typename T>
+        T &catch_var(const std::string &name, T &new_val) {
+            this->var_map[name] = ::tyan::to_string(new_val);
+            return new_val;
+        }
+
+        template<typename T>
+        void log_value(T &new_val) {
+            std::cout << ::tyan::to_string(new_val);
+        }
+
+        void log_value(const std::string &value_str) {
+            std::cout << value_str;
+        }
+
+        void log_var(const std::string &name) {
+            if (this->var_map.count(name)) {
+                std::cout << name << ":" << this->var_map[name];
+            } else {
+                std::cout << name;
+            }
+        }
+
+        void log_line(const std::string &line) {
+            std::string tmp;
+            std::cout << std::string(depth_ << 1, ' ');
+            for (const char c: line) {
+                if (c == ' ' || c == ')' || c == ',') {
+                    this->log_var(tmp);
+                    tmp.clear();
+                    std::cout << c;
+                    continue;
+                }
+                tmp += c;
+            }
+            if (!tmp.empty()) {
+                this->log_var(tmp);
+            }
+            std::cout << std::endl;
+        }
+
+        ~Painter() {
+            // std::cout << os.str() << std::endl;
+        }
+    };
+
+    class PainterDomainGuard {
+        Painter* painter_;
+    public:
+        explicit PainterDomainGuard(Painter* painter): painter_((painter)) {
+            painter->push();
+        }
+
+        ~PainterDomainGuard() {
+            painter_->pop();
+        }
+    };
+
+}
+
+
+#define TyanCatch(var) tyan_painter.catch_var(#var, var)
+#define LogLine(line) tyan_painter.log_line(line)
